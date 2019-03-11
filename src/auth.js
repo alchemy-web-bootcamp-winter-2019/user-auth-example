6f98496b32449dbc0ebd5ca4a2ecb0bbbe00aef8
martypdx <marty@martypdx.com>
2019-03-10 17:08:48 -0700

    basic setup and auth using firebaseui

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,15 @@
 import loadHeader from './header-component.js';
+import { auth } from './firebase.js';
 
 loadHeader();
+
+const ui = new firebaseui.auth.AuthUI(auth);
+
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    signInSuccessUrl: './',
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE
+});
