5c999347d42d4a5e0c7d749fb06b0f84dffb3271
martypdx <marty@martypdx.com>
2019-03-11 16:12:13 -0700

    include import of firebase in test/index.js so travis works :)

--- test/index.js
+++ test/index.js
@@ -7,5 +7,7 @@ const { window } = new JSDOM(``);
 global.window = window;
 global.document = window.document;
 
+global.firebase = require('firebase');
+
 require = require('esm')(module);
 module.exports = require("./tests.js");
