7932d926d25f85d9ecb4b26d817f731d6232ca76
martypdx <marty@martypdx.com>
2019-03-11 10:03:02 -0700

    create two separate template functions for the header

--- src/header-component.js
+++ src/header-component.js
@@ -1,12 +1,10 @@
+import { auth } from './firebase.js';
+
 export function makeHeader() {
     const html = /*html*/`
         <header>
             <img src="assets/alchemy-logo.png" alt="Alchemy Code Lab Logo">
             <h1 class="header-title">Favorite Movies</h1>
-            <div class="profile">
-                <span id="user-name"></span>
-                <button>Sign Out</button>
-            </div>
         </header>
     `;
 
@@ -15,6 +13,35 @@ export function makeHeader() {
     return template.content;
 }
 
-const headerContainer = document.getElementById('header-container');
-const dom = makeHeader();
-headerContainer.appendChild(dom);
+export function makeProfile(user) {
+    const html = /*html*/`
+        <div class="profile">
+            <img src="${user.photoURL}">
+            <span>${user.displayName}</span>
+            <button>Sign Out</button>
+        </div>
+    `;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
+
+function loadHeader() {
+    const headerContainer = document.getElementById('header-container');
+    const dom = makeHeader();
+    headerContainer.appendChild(dom);
+    
+    auth.onAuthStateChanged(user => {
+        if(user) {
+            // there is a user!
+            const userNameDisplay = document.getElementById('user-name');
+            userNameDisplay.textContent = user.displayName;
+            const profileDisplay = document.getElementById('user-profile');
+            profileDisplay.src = user.photoURL;
+        }
+        else {
+            // no user
+        }
+    });
+}
--- test/header-component.test.js
+++ test/header-component.test.js
@@ -1,16 +1,34 @@
-import { makeHeader } from '../src/header-component.js';
+import { makeHeader, makeProfile } from '../src/header-component.js';
 const test = QUnit.test;
 
-test('make header', function(assert) {
+test('make header', assert => {
     const dom = makeHeader();
     assert.htmlEqual(dom, /*html*/`
         <header>
             <img src="assets/alchemy-logo.png" alt="Alchemy Code Lab Logo">
             <h1 class="header-title">Favorite Movies</h1>
-            <div class="profile">
-                <span id="user-name"></span>
-                <button>Sign Out</button>
-            </div>
         </header>
     `);
+});
+
+test('make profile', assert => {
+    // arrange
+    const user = {
+        displayName: 'Marty Nelson',
+        photoURL: 'https://lh5.googleusercontent.com/-EnRNBAYvURk/AAAAAAAAAAI/AAAAAAAAAK8/cMJNFAVX5SE/photo.jpg'
+    };
+
+    const expected = /*html*/`
+        <div class="profile">
+            <img src="https://lh5.googleusercontent.com/-EnRNBAYvURk/AAAAAAAAAAI/AAAAAAAAAK8/cMJNFAVX5SE/photo.jpg">
+            <span>Marty Nelson</span>
+            <button>Sign Out</button>
+        </div>
+    `;
+
+    //act
+    const dom = makeProfile(user);
+
+    //assert
+    assert.htmlEqual(dom, expected);
 });
